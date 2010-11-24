8a30e43d40eb903ff9d4e71bfa2b455d31f3faa5
Pete Browne <me@petebrowne.com>
2010-11-24 11:34:09 -0600

    Updating version

--- dist/classified.js
+++ dist/classified.js
@@ -1,6 +1,6 @@
 //--------------------------------------------------------------------------
 //
-//  Classified.js v0.3.1
+//  Classified.js v0.4
 //  http://github.com/petebrowne/classify
 //
 //  Copyright (c) 2010, Peter Browne

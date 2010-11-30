92a43075440169a484f015b7caf10270a109e58b
Pete Browne <me@petebrowne.com>
2010-11-30 08:49:26 -0600

    Adding Function#curry, #delay, #defer, and #stop

--- dist/classified.js
+++ dist/classified.js
@@ -407,13 +407,62 @@ classify(Function, function() {
   // function is called, it will call the original ensuring that `this` is set
   // to `context`. Also optionally curries arguments for the function.
   def('bind', function(context) {
-    if (context == null) {
+    if (arguments.length < 2 && context == null) {
       return this;
     }
-    var method = this;
+    
+    var method = this,
+        args   = slice.call(arguments, 1);
+        
     return function() {
-      return method.apply(context, arguments);
+      return method.apply(context, args.concat(slice.call(arguments)));
     }
+  });
+  
+  // *Curries* (burns in) arguments to a function, returning a new function
+  // that when called with call the original passing in the curried arguments
+  // (along with any new ones):
+  def('curry', function() {
+    if (!arguments.length) return this;
+    
+    var method = this,
+        args   = slice.call(arguments);
+        
+    return function() {
+      return method.apply(null, args.concat(slice.call(arguments)));
+    }
+  });
+  
+  // Schedules the function to run after the specified amount of time, passing
+  // any arguments given.
+  // 
+  // Behaves much like `window.setTimeout`, but the timeout is in seconds
+  // rather than milliseconds. Returns an integer ID that can be used to
+  // clear the timeout with `window.clearTimeout` before it runs.
+  def('delay', function(timeout) {
+    var method = this,
+        args   = slice.call(arguments, 1);
+        
+    if (timeout <= 0) {
+      return method.apply(null, args);
+    }
+    else {
+      return this.__timeoutID__ = setTimeout(function() {
+        return method.apply(null, args);
+      }, timeout * 1000);
+    }
+  });
+  
+  // Schedules the function to run as soon as the interpreter is idle.
+  def('defer', function() {
+    return this.delay.apply(this, [ 0.01 ].concat(slice.call(arguments)));
+  });
+  
+  // If a #delay, #defer, or #periodical call has been made, calling #stop
+  // will stop the function call from being made.
+  def('stop', function() {
+    clearTimeout(this.__timeoutID__);
+    // clearInterval(this.__intervalID__);
   });
 });
 
--- spec/classified/functionSpec.js
+++ spec/classified/functionSpec.js
@@ -12,5 +12,119 @@ describe('Function', function() {
       expect(method.bind()).toBe(method);
       expect(method.bind(null)).toBe(method);
     });
+    
+    it('curries arguments for the function', function() {
+      var method = function() {
+        return Array.prototype.slice.call(arguments);
+      };
+      
+      expect(method.bind(null, 1, 2, 3)(4, 5)).toEqual([ 1, 2, 3, 4, 5 ]);
+    });
+  });
+  
+  describe('#curry', function() {
+    it('curries arguments for the function', function() {
+      var method = function() {
+        return Array.prototype.slice.call(arguments);
+      };
+      
+      expect(method.curry(1, 2, 3)(4, 5)).toEqual([ 1, 2, 3, 4, 5 ]);
+    });
+  });
+  
+  describe('#delay', function() {
+    it('delays a function by given amount of time', function() {
+      var delayed = false;
+      var method = function() {
+        delayed = true;
+      };
+      
+      method.delay(0.1);
+      expect(delayed).toBe(false);
+      
+      waits(150);
+      runs(function() {
+        expect(delayed).toBe(true);
+      });
+    });
+    
+    it('passes arguments to the delayed function', function() {
+      var args;
+      var method = function() {
+        args = Array.prototype.slice.call(arguments);
+      };
+      
+      method.delay(0.1, 1, 2, 3);
+      expect(args).toBeUndefined();
+      
+      waits(150);
+      runs(function() {
+        expect(args).toEqual([ 1, 2, 3 ]);
+      });
+    });
+  });
+  
+  describe('#defer', function() {
+    it('defers a function', function() {
+      var defered = false;
+      var method = function() {
+        defered = true;
+      };
+      
+      method.defer();
+      expect(defered).toBe(false);
+      
+      waits(50);
+      runs(function() {
+        expect(defered).toBe(true);
+      });
+    });
+    
+    it('passes arguments to the deferred function', function() {
+      var args;
+      var method = function() {
+        args = Array.prototype.slice.call(arguments);
+      };
+      
+      method.defer(1, 2, 3);
+      expect(args).toBeUndefined();
+      
+      waits(50);
+      runs(function() {
+        expect(args).toEqual([ 1, 2, 3 ]);
+      });
+    });
+  });
+  
+  describe('#stop', function() {
+    it('stops delayed calls', function() {
+      var delayed = false;
+      var method = function() {
+        delayed = true;
+      };
+      
+      method.delay(0.1);
+      method.stop();
+      
+      waits(150);
+      runs(function() {
+        expect(delayed).toBe(false);
+      });
+    });
+    
+    it('stops deferred calls', function() {
+      var defered = false;
+      var method = function() {
+        defered = true;
+      };
+      
+      method.defer();
+      method.stop();
+      
+      waits(50);
+      runs(function() {
+        expect(defered).toBe(false);
+      });
+    });
   });
 });
